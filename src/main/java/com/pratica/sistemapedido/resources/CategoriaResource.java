0b9641743e0c31a22b9485cbbe25c7a914d993d4
andreyliporage <andreyliporage@gmail.com>
2020-11-13 11:51:34 -0300

    criando classe de dominio categoria

--- src/main/java/com/pratica/sistemapedido/resources/CategoriaResource.java
+++ src/main/java/com/pratica/sistemapedido/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.pratica.sistemapedido.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pratica.sistemapedido.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String testaRest() {
-		return "REST está funcionando!";
+	public List<Categoria> lista() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }

9e6ce7444f1ca21e09fb258c7589557bce50dfca
andreyliporage <andreyliporage@gmail.com>
2020-11-14 11:33:09 -0300

    criando seed no banco de dados

--- src/main/java/com/pratica/sistemapedido/SistemaPedidoApplication.java
+++ src/main/java/com/pratica/sistemapedido/SistemaPedidoApplication.java
@@ -1,13 +1,31 @@
 package com.pratica.sistemapedido;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pratica.sistemapedido.domain.Categoria;
+import com.pratica.sistemapedido.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SistemaPedidoApplication {
+public class SistemaPedidoApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaPedidoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }

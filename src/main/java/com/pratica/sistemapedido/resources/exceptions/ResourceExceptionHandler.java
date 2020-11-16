5d634b7f5b4f41eac32a7414763a0ca7e9edcea5
andreyliporage <andreyliporage@gmail.com>
2020-11-16 14:18:39 -0300

    ajustes de excecao endpoint /categorias/id

---
+++ src/main/java/com/pratica/sistemapedido/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.pratica.sistemapedido.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.pratica.sistemapedido.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+}

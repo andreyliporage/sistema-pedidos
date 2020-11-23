a2ae1dff397e4607e1b53378adc24047925d1ca3
andreyliporage <andreyliporage@gmail.com>
2020-11-23 11:06:24 -0300

    endpoint para criar categorias

--- src/main/java/com/pratica/sistemapedido/resources/CategoriaResource.java
+++ src/main/java/com/pratica/sistemapedido/resources/CategoriaResource.java
@@ -1,11 +1,16 @@
 package com.pratica.sistemapedido.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pratica.sistemapedido.domain.Categoria;
 import com.pratica.sistemapedido.services.CategoriaService;
@@ -21,5 +26,12 @@ public class CategoriaResource {
 	public ResponseEntity<?> buscar(@PathVariable Integer id) {
 		Categoria categoria = service.buscar(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }

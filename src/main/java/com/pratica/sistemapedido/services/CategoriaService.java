8849033eeab93573c4f92f9efc04cf18d71f46a9
andreyliporage <andreyliporage@gmail.com>
2020-11-23 12:11:50 -0300

    deletando uma categoria com delete e criando excecao dataintegrity

--- src/main/java/com/pratica/sistemapedido/services/CategoriaService.java
+++ src/main/java/com/pratica/sistemapedido/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.pratica.sistemapedido.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.pratica.sistemapedido.domain.Categoria;
 import com.pratica.sistemapedido.repositories.CategoriaRepository;
+import com.pratica.sistemapedido.services.exceptions.DataIntegrityException;
 import com.pratica.sistemapedido.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,5 +30,14 @@ public class CategoriaService {
 	public Categoria update(Categoria categoria) {
 		find(categoria.getId());
 		return repository.save(categoria);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }

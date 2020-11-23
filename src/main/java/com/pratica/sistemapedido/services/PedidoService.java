3540d85b5eb48f64ec4062013e8d47071129ce09
andreyliporage <andreyliporage@gmail.com>
2020-11-23 10:17:04 -0300

    endpoint /pedidos/id

---
+++ src/main/java/com/pratica/sistemapedido/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.pratica.sistemapedido.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pratica.sistemapedido.domain.Cliente;
+import com.pratica.sistemapedido.domain.Pedido;
+import com.pratica.sistemapedido.repositories.PedidoRepository;
+import com.pratica.sistemapedido.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> pedido = repository.findById(id);
+		return pedido.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+}

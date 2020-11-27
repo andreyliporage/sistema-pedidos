a2c55c2f5b4f0f4514a6ccb0fb92ae9554c9590d
andreyliporage <andreyliporage@gmail.com>
2020-11-27 10:08:17 -0300

    validacao de email repetido

--- src/main/java/com/pratica/sistemapedido/repositories/ClienteRepository.java
+++ src/main/java/com/pratica/sistemapedido/repositories/ClienteRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pratica.sistemapedido.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
+	
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/com/pratica/sistemapedido/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/pratica/sistemapedido/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pratica.sistemapedido.domain.Cliente;
 import com.pratica.sistemapedido.domain.enums.TipoCliente;
 import com.pratica.sistemapedido.dto.ClienteNewDTO;
+import com.pratica.sistemapedido.repositories.ClienteRepository;
 import com.pratica.sistemapedido.resources.exceptions.FieldMessage;
 import com.pratica.sistemapedido.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (clienteNewDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(clienteNewDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente clienteEmail =  clienteRepository.findByEmail(clienteNewDto.getEmail());
+		
+		if (clienteEmail != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
